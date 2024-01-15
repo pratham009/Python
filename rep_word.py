76b11e0eb649e29fd05faa1db7d782a811986aa5
Pratham vichare <prathamvichare645@gmail.com>
2024-01-15 15:15:19 -0500

    This is replacement word file of an given input

---
+++ rep_word.py
@@ -0,0 +1,7 @@
+def replace_word():
+    str = input("enter a str to replace a word: ")
+    word_to_repl = input("Enter a word to replace: ")
+    word_replacement = input("enter a word replacement: ")
+    print(str.replace(word_to_repl,word_replacement))
+
+replace_word()
